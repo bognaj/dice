985333a4378c13ad23bb31549bc713d7ecd95021
bogna <bodzia590@gmail.com>
2021-07-30 20:34:19 +0200

    created dice class

--- base.py
+++ base.py
@@ -1,2 +1,11 @@
-print("Hello world")
-# czy to coś zmieni?
+import random as r
+
+class Dice:
+    def __init__(self, id):
+        self.id = id
+        self.score = None
+    def throw(self):
+        self.score = r.randint(1, 6)
+
+if __name__ == "__main__":
+    pass
