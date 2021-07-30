0b4592a0e5a9cc007a7699d886bf517f877fed74
bogna <bodzia590@gmail.com>
2021-07-30 21:15:03 +0200

    extended dice class, added player class

--- base.py
+++ base.py
@@ -1,11 +1,43 @@
 import random as r
 
 class Dice:
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, active = True):
         self.score = None
+        self.active = active
+    def is_active(self):
+        if self.active == True:
+            return True
+        else:
+            return False
     def throw(self):
-        self.score = r.randint(1, 6)
+        if self.is_active:
+            self.score = r.randint(1, 6)
+        else:
+            self.score = None
+        return self.score
+
+    def turn_off(self):
+        self.active = False
+
+class Player:
+    def __init__(self, id, risk):
+        self.id = id
+        self.dice = Dice()
+        self.points = 0
+        self.risk = risk
+        self.num_of_active = 5
+
+    def add_points(self, score):
+        self.points += score
+
+    def throw_dices(self):
+        score_list = []
+        for i in range(0, self.num_of_active):
+            score_list.append(self.dice.throw())
+        return score_list
+
+
 
 if __name__ == "__main__":
-    pass
+    Bodzia = Player(1, 1)
+    print(Bodzia.throw_dices())
