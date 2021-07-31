36ace62786d59113b5ded12c743768dffa03cc86
bogna <bodzia590@gmail.com>
2021-07-31 17:03:49 +0200

    counting points in one throw

--- base.py
+++ base.py
@@ -4,6 +4,7 @@ class Dice:
     def __init__(self, active = True):
         self.score = None
         self.active = active
+
     def is_active(self):
         if self.active == True:
             return True
@@ -37,7 +38,47 @@ class Player:
         return score_list
 
 
+def collect_points(throw_result):
+    points = 0
+    dices_thrown = len(throw_result)
+    possible_scores = [1, 2, 3, 4, 5, 6]
+
+    nums = [throw_result.count(i) for i in possible_scores]
+
+    if 3 in nums:
+        position = nums.index(3)
+        if position == 0:
+            points += 30
+        else:
+            points += 10*possible_scores[position]
+        
+
+    if 4 in nums:
+        position = nums.index(4)
+        if position == 0:
+            points += 100
+        else:
+            points += 10*possible_scores[position]*possible_scores[position]
+        
+
+    if 5 in nums:
+        position = nums.index(5)
+        if position == 0:
+            points += 1000
+        else:
+            points += 10*possible_scores[position]*possible_scores[position]*possible_scores[position]
+
+    if (nums[0] == 1) or (nums[0] == 2):
+        points += nums[0] * 10
+
+    if (nums[4] == 1) or (nums[4] == 2):
+        points += nums[4] * 5
+        
+    return points
+
 
 if __name__ == "__main__":
     Bodzia = Player(1, 1)
-    print(Bodzia.throw_dices())
+    throw = Bodzia.throw_dices()
+    print(throw)
+    print(collect_points(throw))
