e653b787359240d9343504077e117fa9a897eeb0
bogna <bodzia590@gmail.com>
2022-02-09 20:04:47 +0100

    mean score while throwing k dices

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,14 +1,25 @@
+from itertools import count
 from base import collect_points
 import numpy as np
 
-count_list = []
-
-for i in range (100000):
-    throw = list(np.random.randint(1, 7, 5))
-    score = collect_points(throw)
-    if score[0] > 0:
-        count_list.append(1)
-    else:
-        count_list.append(0)
+def if_any_points(k): # probability of throw with points if we have k dices
+    count_list = []
+    for i in range (100000):
+        throw = list(np.random.randint(1, 7, k))
+        score = collect_points(throw)
+        if score[0] > 0:
+            count_list.append(1)
+        else:
+            count_list.append(0)
         
-print(sum(count_list)/len(count_list))
+    return sum(count_list)/len(count_list)
+
+def mean_point_score(k): # mean score while throwing k dices
+    count_list = []
+    for i in range(100000):
+        throw = list(np.random.randint(1, 7, k))
+        score = collect_points(throw)
+        count_list.append(score[0])
+    return np.mean(np.array(count_list))
+
+print(mean_point_score(5))        
